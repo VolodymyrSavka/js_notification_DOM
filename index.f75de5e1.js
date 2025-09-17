9ee928970e41cf6ba8c713f7f0606eac71b6d5bd
Volodymyr Savka <savkavoldemar1@gmail.com>
2025-09-17 20:35:37 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,25 @@
+"use strict";
+const pushNotification = (posTop, posRight, title, description, type)=>{
+    const body = document.body;
+    const orange = document.createElement("div");
+    orange.style.top = posTop + "px";
+    orange.style.right = posRight + "px";
+    const orangeTitle = document.createElement("h2");
+    orangeTitle.classList.add("title");
+    orangeTitle.textContent = title;
+    const orangeMessage = document.createElement("p");
+    orangeMessage.textContent = description;
+    orange.appendChild(orangeTitle);
+    orange.appendChild(orangeMessage);
+    orange.classList.add(type);
+    orange.classList.add("notification");
+    body.appendChild(orange);
+    setTimeout(()=>{
+        orange.style.display = "none";
+    }, 2000);
+};
+pushNotification(10, 10, "Title of Success message", "Message example.\n Notification should contain title and description.", "success");
+pushNotification(150, 10, "Title of Error message", "Message example.\n Notification should contain title and description.", "error");
+pushNotification(290, 10, "Title of Warning message", "Message example.\n Notification should contain title and description.", "warning");
+
+//# sourceMappingURL=index.f75de5e1.js.map
