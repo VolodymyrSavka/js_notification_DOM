31f259ddf7f1a9f1e5cfa90ef4b69c90fa288d70
Volodymyr Savka <savkavoldemar1@gmail.com>
2025-09-17 19:40:46 +0300

    add task test

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,33 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const body = document.body;
+  const orange = document.createElement('div');
+
+  orange.style.position = 'fixed';
+  orange.style.top = posTop;
+  orange.style.right = posRight;
+
+  const orangeTitle = document.createElement('h2');
+
+  orangeTitle.classList.add('title');
+
+  orangeTitle.textContent = title;
+
+  const orangeMessage = document.createElement('p');
+
+  orangeMessage.textContent = description;
+
+  orange.appendChild(orangeTitle);
+  orange.appendChild(orangeMessage);
+
+  orange.classList.add(type);
+  orange.classList.add('notification');
+  body.appendChild(orange);
+
+  setTimeout(() => {
+    orange.style.display = 'none';
+  }, 2000);
 };
 
 pushNotification(
